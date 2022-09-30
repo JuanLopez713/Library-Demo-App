f492ea24b3fed1d250d6fa6f4f5ba8c0fed1f7c5
Juan Lopez <juanlpz713@gmail.com>
2022-09-30 12:36:08 -0700

    Initial Commit

---
+++ Library Demo App/Library_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Library_Demo_AppApp.swift
+//  Library Demo App
+//
+//  Created by Juan Lopez on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Library_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
